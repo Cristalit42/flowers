197c30b6afdcd7ca180baebdab8412430216d2f8
FantomIT <fantomchik.pubg.m@gmail.com>
2025-04-03 04:31:16 +0600

    add cart

--- index.4ad95d1b.js
+++ index.4ad95d1b.js
@@ -12872,5 +12872,36 @@ const $831f8f56f2360669$var$sliderMain = new (0, $4e95c04dd8a5890d$export$25ce5a
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    const overlay = document.querySelector('.overlay');
+    const overlayContent = document.querySelector('.overlay__content-wrapper');
+    const overlayBtn = document.querySelectorAll('.js-overlay__btn-open');
+    const overlayBtnClose = document.querySelector('.js-overlay__btn-close');
+    const form = overlay.querySelector('.form');
+    overlayBtn.forEach((btn)=>{
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            overlay.classList.toggle('overlay--hidden');
+        });
+    });
+    overlayBtnClose.addEventListener('click', function() {
+        overlay.classList.add('overlay--hidden');
+    });
+    // Закрытие при клике вне попапа
+    overlay.addEventListener('click', function(e) {
+        if (!overlayContent.contains(e.target)) overlay.classList.add('overlay--hidden');
+    });
+    // Симуляция отправки формы
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        // Эмуляция процесса отправки (например, с задержкой)
+        setTimeout(()=>{
+            overlay.classList.add('overlay--hidden');
+            form.reset(); // Очистка формы после отправки
+        }, 1000);
+    });
+});
+
+
 
 
